ba9041431335e1becaeb8e6d3d8aadb700877c83
W1nt0r <j4n.winter@gmail.com>
2014-11-01 19:38:01 +0100

    Removed unused Comments

--- Schatzkarte/ViewController.swift
+++ Schatzkarte/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Schatzkarte
 //
-//  Created by Jan Winter on 15.10.14.
+//  Created by Jan Winter und Adrian Tang on 15.10.14.
 //  Copyright (c) 2014 HSR. All rights reserved.
 //
 
@@ -11,11 +11,9 @@ import UIKit
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var map: RMMapView!
-    //let mapSource: RMTileSource = RMMBTilesSource(tileSetResource: "hsr", ofType: "mbtiles")
     var mapSource: RMTileSource!
     let hsrCoords: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 47.223252, longitude: 8.817011)
     let app: UIApplication = UIApplication.sharedApplication()
-    //var topGuide: UILayoutSupport!
     var locManager:CLLocationManager!
     var markerIndex: Int!
     var solutionLogger: SolutionLogger!
@@ -24,7 +22,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.topGuide = self.topLayoutGuide
         
         self.mapSource = RMMBTilesSource(tileSetResource: "hsr", ofType: "mbtiles")
         
@@ -39,8 +36,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.initMarkerProgress()
         
         self.loadMarker()
-        
-        //self.createLayout()
     }
     
     func initMap() {
@@ -56,13 +51,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locManager = CLLocationManager()
         locManager.delegate = self
         locManager.desiredAccuracy = kCLLocationAccuracyBest
-        //locManager.distanceFilter = 1
     }
     
     func initMarkerProgress() {
         self.markerProgress.hidesWhenStopped = true
-        //self.markerProgress.color = UIColor(red: 194, green: 0, blue: 0, alpha: 1)
-        //self.view.bringSubviewToFront(self.markerProgress)
         self.progressContainer.hidden = true
         self.progressContainer.layer.cornerRadius = 10
         self.view.bringSubviewToFront(self.progressContainer)
@@ -120,22 +112,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        //locManager.startUpdatingLocation()
     }
     
     @IBAction func setMarker(sender: AnyObject) {
-        /*var marker:RMPointAnnotation = RMPointAnnotation(mapView: self.map, coordinate: CLLocationCoordinate2D(latitude: 47.223252, longitude: 8.817011), andTitle: "Marker")
-        var marker2:RMPointAnnotation = RMPointAnnotation(mapView: self.map, coordinate: CLLocationCoordinate2D(latitude: 8.817011, longitude: 47.223252), andTitle: "Marker2")
-        /*marker2.coordinate = CLLocationCoordinate2D(latitude: 8.817011, longitude: 47.223252)
-        marker.coordinate = CLLocationCoordinate2D(latitude: 47.223252, longitude: 8.817011)
-        marker.mapView = self.map
-        marker2.mapView = self.map*/
-        self.map.addAnnotation(marker)
-        self.map.addAnnotation(marker2)
-        println(self.map.annotations)*/
         if self.isLocServicesEnabled()  {
             locManager.startUpdatingLocation()
-            //self.markerProgress.startAnimating()
             self.startCustomMProgressAnimation()
         } else {
             var alert=UIAlertController(title:"Ortungsdienste einschalten",message:"Bitte die Ortungsdienste einschalten.",preferredStyle:UIAlertControllerStyle.Alert);
@@ -161,13 +142,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        //println(locations)
-        //println(manager.location.timestamp)
-        //println(manager.location.timestamp.timeIntervalSinceNow)
-        //println(locations.last)
-        //println(manager.location)
         let locAge: NSTimeInterval = -manager.location.timestamp.timeIntervalSinceNow
-        println(locAge)
         
         if locAge > 6.0 {
             return
@@ -176,10 +151,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if manager.location.horizontalAccuracy < 0 {
             return
         }
-        
-        println(manager.location.horizontalAccuracy)
-        //println(manager.desiredAccuracy)
-        println(self.locManager.desiredAccuracy)
         
         if manager.location.horizontalAccuracy <= 20.0 {
             self.locManager.stopUpdatingLocation()
@@ -190,8 +161,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             marker.mapView = self.map
             self.map.addAnnotation(marker)
             
-            println(map.annotations)
-            
             let userDefaults = NSUserDefaults.standardUserDefaults()
             
             var markerToSave : [NSObject : AnyObject] = [
@@ -199,13 +168,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 "lon" : manager.location.coordinate.longitude
             ]
             
-            println("Lat: \(manager.location.coordinate.latitude)")
-            println("Lon: \(manager.location.coordinate.longitude)")
-            
             userDefaults.setObject(markerToSave, forKey: uKey)
             var success:Bool = userDefaults.synchronize()
             
-            //self.markerProgress.stopAnimating()
             self.stopCustomMProgressAnimation()
             
             if !success {
@@ -220,18 +185,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    //func getMarker() -> Dictionary<String, [NSObject: AnyObject]>? {
     func getMarker() -> Array<[NSObject: AnyObject]>? {
         let userDefaults = NSUserDefaults.standardUserDefaults()
-        var uDDictionary: [NSObject: AnyObject] = userDefaults.dictionaryRepresentation()
-        //var markerCollection = Dictionary<String, [NSObject: AnyObject]>()
+        let uDDictionary: [NSObject: AnyObject] = userDefaults.dictionaryRepresentation()
         var markerCollection = Array<[NSObject: AnyObject]>()
         
         for (key, value) in uDDictionary {
-            var strKey = key as NSString
+            let strKey = key as NSString
+            let keyStr = String(strKey)
             
-            if startsWith(String(strKey), "Marker") {
-                //markerCollection[String(strKey)] = value as? [NSObject: AnyObject]
+            if startsWith(keyStr, "Marker") {
                 markerCollection.append(value as [NSObject: AnyObject])
             }
         }
@@ -244,38 +207,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func logSolution(sender: AnyObject) {
-        /*let userDefaults = NSUserDefaults.standardUserDefaults()
-        var uDDictionary: [NSObject: AnyObject] = userDefaults.dictionaryRepresentation()
-        //println(uDDictionary)
-        //println(uDDictionary.values.array)
-        //var markerCollection: [NSObject: AnyObject]!
-        //var markerCollection : [String: [NSObject: AnyObject]]!
-        var markerCollection = Dictionary<String, [NSObject: AnyObject]>()
-        
-        for (key, value) in uDDictionary {
-            var strKey = key as NSString
-            //println(strKey)
-            
-            if startsWith(String(strKey), "Marker") {
-                
-                
-                println(String(strKey))
-                
-                println(value)
-                markerCollection[String(strKey)] = value as? [NSObject: AnyObject]
-                
-            }
-        }
-        
-        println(markerCollection)
-        println(markerCollection.isEmpty)*/
-        
         if let markerCollection = self.getMarker() {
-            //println(self.parseJSON(markerCollection))
-            //println(self.parseMikrograd(markerCollection))
             if let solution = self.parseJSON(self.parseMikrograd(markerCollection)) {
-                self.solutionLogger.logSolution("Schatzkarte", taskName: solution)
-                //println(solution)
+                self.solutionLogger.logSolution(solution, taskName: "Schatzkarte")
             }
         }
     }
@@ -309,30 +243,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func prefersStatusBarHidden() -> Bool {
         return false
     }
-    
-    /*override func supportedInterfaceOrientations() -> Int {
-        return Int(UIInterfaceOrientationMask.All.toRaw())
-    }
-    
-    func createLayout() {
-        
-
-        //map.setTranslatesAutoresizingMaskIntoConstraints(false)
-        
-        
-        
-        /*let viewsDictionary = ["map": self.map, "topGuide": self.topGuide]
-        
-        let map_constraint_H:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[map]-0-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
-        let map_constraint_V:NSArray = NSLayoutConstraint.constraintsWithaVisualFormat("V:|-0-[topGuide]-0-[map]-0-|", options: NSLayoutFormatOptions.AlignAllLeading, metrics: nil, views: viewsDictionary)
-        
-        self.view.addConstraints(map_constraint_H)
-        self.view.addConstraints(map_constraint_V)*/
-    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
